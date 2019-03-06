d5baaf6c45524ac4600180d5f2bceffd87bba1d0
Kieron Browne <kbrowne@pivotal.io>
2019-03-06 22:40:56 +0000

    starting react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: "..."};
+  }
+
+  componentDidMount() {
+    const apiUrl = process.env.REACT_APP_API_URL;
+    fetch(apiUrl)
+      .then(resp => resp.json())
+      .then(myJson => {
+        console.log(myJson);
+        this.setState({name: myJson.name})
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        Message: {this.state.name}
       </div>
     );
   }
